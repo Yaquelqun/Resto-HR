3cde7e99b0d875dfdd974a5b44cf2d892dcee7b1
Yaquelqun <sandjiv.parassou@gmail.com>
2024-05-03 17:15:36 +0200

    add feedback state inclusion validation

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,7 +1,14 @@
 class Feedback < ApplicationRecord
+  INTENDED = 'intended'.freeze
+  PLANNED = 'planned'.freeze
+  GIVEN = 'given'.freeze
+  STATES = [INTENDED, PLANNED, GIVEN].freeze
+
   belongs_to :participation
   has_one :user, through: :participation
   has_one :resto, through: :participation
-  
+
   belongs_to :target, class_name: 'User'
+
+  validates :state, inclusion: { in: STATES }
 end
