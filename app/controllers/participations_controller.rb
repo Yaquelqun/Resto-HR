fee5929940017b16d8dddc661f015348be5fa757
Yaquelqun <sandjiv.parassou@gmail.com>
2024-05-03 17:01:58 +0200

    css shennanigans

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -3,4 +3,10 @@ class ParticipationsController < ApplicationController
   def index
     @participations = current_user.participations.includes(:resto)
   end
+
+  def create
+    Participation.create!(user_id: current_user.id, resto_id: params[:resto_id])
+    flash[:success] = "Participation registered"
+    redirect_to restos_path
+  end
 end
