28c085b945f129986115ea179300630575d8731a
Yaquelqun <sandjiv.parassou@gmail.com>
2024-05-17 13:39:23 +0200

    humanized simple resto infos

--- app/models/resto.rb
+++ app/models/resto.rb
@@ -2,4 +2,8 @@ class Resto < ApplicationRecord
   has_many :participations
   has_many :users, through: :participations
   has_many :feedbacks, through: :participations
+
+  def humanized_description
+    date.strftime("%A, %B %d, %Y") + " participants: #{users.count}"
+  end
 end
