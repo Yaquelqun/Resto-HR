7ddeb2e949c85ba977c3e9b297f9842647fa977f
Yaquelqun <sandjiv.parassou@gmail.com>
2024-07-12 15:40:41 +0200

    make feedback selection scoped to the reso users + fix seed

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,4 +2,8 @@ class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :resto
   has_many :feedbacks, dependent: :destroy
+
+  def siblings
+    Participation.where(resto_id: resto_id).where.not(id: id)
+  end
 end
