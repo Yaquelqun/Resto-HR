954e8fbbed08a2180c48f1cc1771850a43e0c630
Yaquelqun <sandjiv.parassou@gmail.com>
2025-03-07 14:15:55 +0100

    more objects in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-
-users = [{ pseudo: "Antoine", email: "antoine@ringtwice.com" }, 
+puts "Generating Users"
+[{ pseudo: "Antoine", email: "antoine@ringtwice.com" }, 
 { pseudo: "Benjamin", email: "benjamin@ringtwice.com" }, 
 { pseudo: "Blanche", email: "blanche@ringtwice.com" }, 
 { pseudo: "Carl", email: "carl@ringwice.com" }, 
@@ -24,6 +24,22 @@ users = [{ pseudo: "Antoine", email: "antoine@ringtwice.com" },
 { pseudo: "Sandjiv", email: "sandjiv@ringtwice.com", role: 'admin' }, 
 { pseudo: "Sébastien", email: "sebastieng@ringtwice.com" }, 
 { pseudo: "Ségolène", email: "segolene@ringtwice.com" }, 
-{ pseudo: "Tom", email: "tom@ringtwice.com" }]
+{ pseudo: "Tom", email: "tom@ringtwice.com" }].each { User.create(email: _1[:email], pseudo: _1[:pseudo], password: 'ringtwice') }
 
-users.each { User.create(email: _1[:email], pseudo: _1[:pseudo], password: 'ringtwice') }
+puts "Generating Restos"
+[ { date: Date.today }, { date: Date.tomorrow }, { date: Date.tomorrow + 1.day }, { date: Date.tomorrow + 2.days }, { date: Date.tomorrow + 3.days } ].each { Resto.create(date: _1[:date]) }
+
+puts "Generating Participations"
+Resto.all.each do |resto|
+  User.all.sample(6).each { Participation.create(user: _1, resto: resto) }
+end
+
+puts "Generating MeetingIntents"
+Resto.includes(:users).all.each do |resto|
+  user_ids = resto.users.pluck(:id)
+  user_ids.sample(4).each do |user_id|
+    MeetingIntent.create(resto: resto, target_id: (user_ids - [user_id]).sample, initiator_id: user_id)
+  end
+end
+
+puts "Seeds generated"
