5a9c86184c67b03da2b7b641ca417b3d8f777b27
Yaquelqun <sandjiv.parassou@gmail.com>
2025-03-21 16:17:57 +0100

    add possibility to have meeting creation

--- app/controllers/restos/meetings_controller.rb
+++ app/controllers/restos/meetings_controller.rb
@@ -2,8 +2,8 @@ module Restos
   class MeetingsController < ApplicationController
 
     def create
-      resto = Resto.find(params[:id])
-      fail!
+      resto = Resto.find(params[:resto_id])
+      flash[:notice] = "Meeting created!"
       redirect_to resto_path(resto)
     end
   end
